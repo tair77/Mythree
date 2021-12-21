e34e6f7240a2db4c7b2f618bf5f1a2a470e140ac
tair77 <tair.baratov77@gmail.com>
2021-12-21 17:33:45 +0600

    добаваил true,false

--- app/src/main/java/com/example/myapplication3/GameActivity.java
+++ app/src/main/java/com/example/myapplication3/GameActivity.java
@@ -1,18 +1,19 @@
 package com.example.myapplication3;
 
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class GameActivity extends AppCompatActivity {
     ImageView imImage;
     Button btn1, btn2, btn3;
     Button btnOne, btnTwo, btnThree;
+    boolean isOleni, isBelka, isDog;
 
 
     @Override
@@ -21,12 +22,10 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         initialization();
         listeners();
+
     }
 
-
-
     private void initialization() {
-
         imImage = findViewById(R.id.in_image);
         btnOne = findViewById(R.id.btn_one);
         btnTwo = findViewById(R.id.btn_two);
@@ -34,48 +33,68 @@ public class GameActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn_1);
         btn2 = findViewById(R.id.btn_2);
         btn3 = findViewById(R.id.btn_3);
+
     }
 
-    public void changeImage (View view){
-        switch (view.getId()){
+    public void changeImage(View view) {
+        switch (view.getId()) {
             case R.id.btn_1:
                 imImage.setImageResource(R.drawable.oleni);
+                isOleni = true;
+                isBelka = false;
+                isDog = false;
                 break;
             case R.id.btn_2:
                 imImage.setImageResource(R.drawable.belka);
+                isBelka = true;
+                isOleni = false;
+                isDog = false;
                 break;
             case R.id.btn_3:
                 imImage.setImageResource(R.drawable.dog);
+                isDog = true;
+                isBelka = false;
+                isOleni = false;
                 break;
         }
+
     }
 
     private void listeners() {
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view
+            public void onClick(View v
             ) {
-                Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_LONG).show();
-
-
+                if (isOleni) {
+                    Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(GameActivity.this, "НЕ правильно", Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
         btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view
-            ) {
-                Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                if (isBelka) {
+                    Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(GameActivity.this, "НЕ правильно", Toast.LENGTH_LONG).show();
+
             }
         });
         btnThree.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view
-            ) {
-                Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                if (isDog) {
+                    Toast.makeText(GameActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(GameActivity.this, "НЕ правильно", Toast.LENGTH_LONG).show();
+
             }
         });
+
     }
 
-    public void changImage(View view) {
-    }
+
 }
--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,22 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initialization();
         listeners();
+
     }
-
-
 
     private void initialization() {
         btnStart = findViewById(R.id.btn_start);
     }
-
-
-    private void listeners() {
+    private void listeners(){
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(intent);
-
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this , GameActivity.class);
+                startActivity( intent);
+                Log.e("TAG", "onClick: " + intent);
             }
         });
     }
